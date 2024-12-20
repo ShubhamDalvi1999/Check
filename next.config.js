cb708b320bc454aea31ff9d5cbdb1fd9bb8695b9
ShubhamDalvi1999 <dshubhamp1999@gmail.com>
2024-12-20 00:40:10 +0000

    package manager changed

--- next.config.js
+++ next.config.js
@@ -11,6 +11,13 @@ const nextConfig = {
       test: /\.css$/,
       use: ['style-loader', 'css-loader', 'postcss-loader'],
     });
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/fonts/[name][ext]',
+      },
+    });
     return config;
   },
 }
