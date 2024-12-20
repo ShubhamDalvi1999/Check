bb12e51657ef0bae647f1446fe7fee99c5eec46a
ShubhamDalvi1999 <dshubhamp1999@gmail.com>
2024-12-20 01:49:40 +0000

    Working Deployment Stage

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,9 @@ export default defineConfig(({ mode }) => ({
         main: path.resolve(__dirname, 'index.html'),
       },
       output: {
-        assetFileNames: './assets/[name].[hash][extname]',
-        chunkFileNames: './assets/[name].[hash].js',
-        entryFileNames: './assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
       },
     },
   },
